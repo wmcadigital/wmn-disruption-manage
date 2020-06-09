fb89c9f7c5ebec7cf92bfcf23b1164b123ad75bf
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-06-09 11:17:18 +0100

    App launches with correct settings

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <div className="wmnds-col-1 wmnds-col-md-3-5">hello</div>
+      <div className="wmnds-col-1 wmnds-col-md-1-5">side</div>
+    </>
+  );
 }
 
 export default App;
