cee10111378e9235f2179e9668d9d69435537562
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-06-15 12:20:47 +0100

    Added portal url

--- src/components/TileLayout/SummaryTile/SummaryTile.js
+++ src/components/TileLayout/SummaryTile/SummaryTile.js
@@ -3,9 +3,13 @@ import { SubscriberContext } from 'globalState/SubscriberContext';
 
 const SummaryTile = () => {
   const [subscriberState] = useContext(SubscriberContext);
+  const portalURL = `${window.location.protocol}//${window.location.host}/?user=${subscriberState.query.user}`;
 
   return (
     <div className="wmnds-content-tile wmnds-col-1">
+      <p>
+        My portal URL is: <a href={portalURL}>{portalURL}</a>
+      </p>
       <p>Managing {subscriberState.user.name}&apos;s alerts about disruption</p>
       <p>Alerts sent to {subscriberState.user.email}</p>
     </div>
