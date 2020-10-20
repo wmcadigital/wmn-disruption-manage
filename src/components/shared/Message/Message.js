df920e9fd0ed3713773491f0872dc284126d809e
Catia Costa <catia.rodrigues.costa@gmail.com>
2020-10-20 10:49:29 +0100

    Update existing components.

--- src/components/shared/Message/Message.js
+++ src/components/shared/Message/Message.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Icon from 'components/shared/Icon/Icon';
 
-const Message = ({ type, title, message, className }) => {
+const Message = ({ type, title, message, className, hasCloseButton }) => {
   let iconName;
   switch (type) {
     case 'error':
@@ -20,6 +20,12 @@ const Message = ({ type, title, message, className }) => {
 
   return (
     <div className={`wmnds-msg-summary wmnds-msg-summary--${type} ${className}`}>
+      {hasCloseButton && (
+        <button type="button" className="wmnds-msg-summary__close wmnds-link">
+          Close
+          <Icon iconName="general-cross" className="" />
+        </button>
+      )}
       <div className="wmnds-msg-summary__header">
         <Icon iconName={`general-${iconName}`} className="wmnds-msg-summary__icon" />
         <h3 className="wmnds-msg-summary__title">{title}</h3>
@@ -35,9 +41,11 @@ Message.propTypes = {
   type: PropTypes.string,
   title: PropTypes.string,
   message: PropTypes.string,
+  hasCloseButton: PropTypes.bool,
 };
 
 Message.defaultProps = {
+  hasCloseButton: false,
   className: null,
   type: 'success',
   title: 'Good service',
