819f445c3d74b142d65f2d742b8121186904b5d9
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-11-10 12:04:41 +0000

    Updated sign up url to use env vars

--- src/components/TileLayout/SignUpSMSTrialTile/SignUpSMSTrialTile.js
+++ src/components/TileLayout/SignUpSMSTrialTile/SignUpSMSTrialTile.js
@@ -7,14 +7,6 @@ const SignUpSMSTrialTile = ({ setIsDismissTrialActive }) => {
   const [subscriberState] = useContext(SubscriberContext);
   const { name, email } = subscriberState.user;
 
-  let signUpPageUrl;
-  if (process.env.NODE_ENV === 'development') {
-    signUpPageUrl = 'https://deploy-preview-126--wmn-disruption-sign-up.netlify.app/';
-  } else if (process.env.NODE_ENV === 'production') {
-    // signUpPageUrl = 'https://disruption-sign-up.wmnetwork.co.uk/'; {/* TODO : UPDATE LINK before go live */}
-    signUpPageUrl = 'https://deploy-preview-126--wmn-disruption-sign-up.netlify.app/'; // for testing only
-  }
-
   return (
     <div className="wmnds-content-tile wmnds-m-t-lg wmnds-col-1">
       <h2> Sign up to the text message service disruption trial</h2>
@@ -23,7 +15,9 @@ const SignUpSMSTrialTile = ({ setIsDismissTrialActive }) => {
       <div className="wmnds-grid wmnds-grid--align-stretch wmnds-grid--spacing-sm-1-xsm wmnds-grid--spacing-md-2-sm wmnds-grid--spacing-lg-2-sm wmnds-m-t-md">
         <div className="wmnds-col-1 wmnds-col-md-1-2">
           <a
-            href={`${signUpPageUrl}?name=${encodeURI(name)}&email=${email}`}
+            href={`${process.env.REACT_APP_DISRUPTION_SIGN_UP_URL}?name=${encodeURI(
+              name
+            )}&email=${email}`}
             title="Sign up to text message trial"
             target="_blank"
             rel="noopener noreferrer"
